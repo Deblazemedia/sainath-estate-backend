0aadd2655e6585608ec47b533964d1e540419fa0
De Blaze Media <blazemedia29@gmail.com>
2025-09-26 13:08:36 +0530

    Added Property Submission Checks 2

--- models/CreativeImage.js
+++ models/CreativeImage.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const creativeSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    place: { type: String, required: true },
+    subTitle: { type: String, required: true },
     image: { type: String, required: true }
 }, {
     timestamps: true
 });
 
 module.exports = mongoose.model('Creative', creativeSchema);
-
